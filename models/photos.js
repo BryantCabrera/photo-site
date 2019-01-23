d2e6f8b56aa43a88e1d0999182356a2d5b604e4a
Bryant <cabrera.bryant@gmail.com>
2019-01-22 20:11:38 -0800

    Creates photos Show view page.

--- models/photos.js
+++ models/photos.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
     url: {type: String, required: true},
-    title: {type: String, required: true},
     user: {type: String, required: true},
+    title: {type: String, required: true},
     description: String
 });
 
